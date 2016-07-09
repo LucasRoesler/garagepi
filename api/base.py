2b3ac61b90bb31381b31e9606c833884e1483ac5
Lucas Roesler <lucas@enderlabs.com>
2016-07-09 15:42:41 -0600

    Add led circuit test endpoint
    **What**
    - Add endpoint that allows you to control a simple LED test circuit that
      is on GPIO 18.

--- api/base.py
+++ api/base.py
@@ -1,6 +1,10 @@
 """Hug API (local and HTTP access)"""
 
 import hug
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
 
 @hug.get(examples='name=Timothy&age=26')
@@ -9,3 +13,17 @@ def happy_birthday(name: hug.types.text, age: hug.types.number = 1, hug_timer=3)
     """Says happy birthday to a user."""
     return {'message': 'Happy {0} Birthday {1}! I think'.format(age, name),
             'took': float(hug_timer)}
+
+
+@hug.get(examples='output=0')
+@hug.local()
+def led_test(output: hug.types.number = 0, hut_timer=3):
+    """Test the LED circuit attached to the RaspberryPi."""
+    if output < 1:
+        GPIO.output(18, GPIO.LOW)
+    else:
+        GPIO.output(18, GPIO.HIGH)
+
+    state = GPIO.input(18)
+
+    return {'status': state}
